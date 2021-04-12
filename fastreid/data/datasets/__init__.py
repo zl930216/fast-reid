e61d93d5beca92d12547b504ca509c247bfd9680
zl930216 <532165801@qq.com>
2021-04-12 14:37:50 +0800

    add marvel2016

--- fastreid/data/datasets/__init__.py
+++ fastreid/data/datasets/__init__.py
@@ -16,13 +16,12 @@ It must returns an instance of :class:`Backbone`.
 from .cuhk03 import CUHK03
 from .dukemtmcreid import DukeMTMC
 from .market1501 import Market1501
+from .marvel2016 import Marvel2016
 from .msmt17 import MSMT17
 from .AirportALERT import AirportALERT
 from .iLIDS import iLIDS
 from .pku import PKU
 from .prai import PRAI
-from .prid import PRID
-from .grid import GRID
 from .saivt import SAIVT
 from .sensereid import SenseReID
 from .sysu_mm import SYSU_mm
@@ -39,5 +38,6 @@ from .cuhk_sysu import cuhkSYSU
 from .veri import VeRi
 from .vehicleid import VehicleID, SmallVehicleID, MediumVehicleID, LargeVehicleID
 from .veriwild import VeRiWild, SmallVeRiWild, MediumVeRiWild, LargeVeRiWild
+
 
 __all__ = [k for k in globals().keys() if "builtin" not in k and not k.startswith("_")]
